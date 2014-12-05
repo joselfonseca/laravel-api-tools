179594c88b29979fe4f63eafcd471b6edd322a80
Jose Luis Fonseca <desarrollo@reddesignsystems.com>
2014-12-05 10:38:56 -0500

    Simple Crud Trait

--- src/Joselfonseca/LaravelApiTools/Traits/ModelSimpleCrudTrait.php
+++ src/Joselfonseca/LaravelApiTools/Traits/ModelSimpleCrudTrait.php
@@ -1,47 +1,88 @@
 <?php
 
-
 namespace Joselfonseca\LaravelApiTools\Traits;
 
 use Joselfonseca\LaravelApiTools\Exceptions\ValidationException;
 use Joselfonseca\LaravelApiTools\ApiToolsResponder;
-use Illuminate\Validation\Factory as LaravelValidator;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 /**
  * Simple Crud Functions for Eloquent Models
  * @author Jose Fonseca <jose@ditecnologia.com>
  * @package Joselfonseca\LaravelApiTools
  */
-
 trait ModelSimpleCrudTrait {
-    
-    public $validationRules = [];
-    
+
     /**
      * Create a resource
      * @param type $input
      * @return type
      */
-    public function CreateResource($input){
-        try{
+    public function CreateResource($input) {
+        try {
             $this->validate($input);
             $model = $this->create($input);
-        }catch(ValidationException $e){
+        } catch (ValidationException $e) {
+            if (\Config::get('laravel-api-tools::responder::responder') === "dingo") {
+                throw new \Dingo\Api\Exception\StoreResourceFailedException(\Config::get('laravel-api-tools::ValidationCreationErrorMessage'), $e->validator->errors());
+            }
             return ApiToolsResponder::validationError($e->validator);
         }
         return $model;
     }
+
     /**
      * Validate input
      * @param type $input
      * @throws ValidationException
      */
-    public function validate($input){
-        $validator = new LaravelValidator;
-        $validation = $validator->make($input, $this->rulvalidationRuleses);
-        if($validation->fails()){
+    public function validate($input) {
+        if (!isset($this->validationRules)) {
+            $this->validationRules = [];
+        }
+        $validation = \Validator::make($input, $this->validationRules);
+        if ($validation->fails()) {
             throw new ValidationException($validation);
         }
     }
-    
+    /**
+     * Update a resource
+     * @param type $id
+     * @param type $input
+     * @return type
+     * @throws \Dingo\Api\Exception\StoreResourceFailedException
+     */
+    public function updateResource($id, $input) {
+        try {
+            $this->validate($input);
+            $model = $this->findOrFail($id);
+            foreach ($input as $key => $value){
+                $model->{$key} = $value;
+            }
+            $model->save();
+        } catch (ValidationException $e) {
+            if (\Config::get('laravel-api-tools::responder::responder') === "dingo") {
+                throw new \Dingo\Api\Exception\StoreResourceFailedException(\Config::get('laravel-api-tools::ValidationCreationErrorMessage'), $e->validator->errors());
+            }
+            return ApiToolsResponder::validationError($e->validator);
+        } catch(ModelNotFoundException $e){
+            return ApiToolsResponder::resourceNotFound(\Config::get('laravel-api-tools::ResourceNotFound'));
+        }
+        return $model;
+    }
+    /**
+     * Delete a resource
+     * @param type $id
+     * @return type
+     */
+    public function deleteResource($id){
+        try {
+            $model = $this->findOrFail($id);
+            $model->delete();
+        } catch(ModelNotFoundException $e){
+            return ApiToolsResponder::resourceNotFound(\Config::get('laravel-api-tools::ResourceNotFound'));
+        }
+        return ApiToolsResponder::simpleJson(['message' => \Config::get('laravel-api-tools::ResourceDeleted')]);
+    }
+
 }
---
+++ src/config/config.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Responder
+    |--------------------------------------------------------------------------
+    |
+    | Since we now support Dingo API, you can choose to use the 
+    | old responder or dingo api, values are default and dingo
+    |
+    */
+
+    'responder' => 'dingo',
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Error Messages
+    |--------------------------------------------------------------------------
+    |
+    | Validation when creating a resource
+    | 
+    |
+    */
+
+    'ValidationCreationErrorMessage' => 'Error creating the resource',
+    'ResourceNotFound' => 'We could not found what you are looking for',
+    'ResourceDeleted' => 'The model has been deleted'
+];
+
+
