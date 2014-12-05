9434f3c2c69f750e60d7195b4abcd7910b20ba49
Jose Luis Fonseca <desarrollo@reddesignsystems.com>
2014-12-05 15:53:20 -0500

    Or Where sintax column(value|=|or)

---
+++ src/Joselfonseca/LaravelApiTools/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Joselfonseca\LaravelApiTools\Exceptions;
+
+/**
+ * Description of InvalidArgumentException
+ *
+ * @author desarrollo
+ */
+class InvalidArgumentException extends \Exception{
+    
+    
+    
+}
--- src/Joselfonseca/LaravelApiTools/Traits/DingoResponderTrait.php
+++ src/Joselfonseca/LaravelApiTools/Traits/DingoResponderTrait.php
@@ -2,17 +2,20 @@
 
 namespace Joselfonseca\LaravelApiTools\Traits;
 
+use Joselfonseca\LaravelApiTools\Exceptions\InvalidArgumentException;
+
 /**
  * Responses for DingoApi
  * @author Rigo B Castro
  * @author Jose Fonseca <jose@ditecnologia.com>
  */
 trait DingoResponderTrait {
-    
+
     protected $searchParam = 'search';
     protected $searchFirstDelimiter = ':';
     protected $searchSecondDelimiter = '|';
     protected $searchDefaultCondition = '=';
+    protected $whereTypeDefault = 'AND';
 
     // ----------------------------------------------------------------------
 
@@ -65,32 +68,39 @@ trait DingoResponderTrait {
 
     protected function parseSearchParams($model) {
         $modelReturn = $model;
-
         if (\Input::has($this->searchParam)) {
             $search = \Input::get($this->searchParam);
-
             $searchExplode = explode($this->searchFirstDelimiter, $search);
-
             foreach ($searchExplode as $searchExplodeItem) {
-                preg_match_all('/([\w]+)\(([^\)]+)\)/', $searchExplodeItem, $allModifiersArr);
-
-                $column = $allModifiersArr[1][0];
-                $valueCond = explode($this->searchSecondDelimiter, $allModifiersArr[2][0], 2);
-
-                if (!empty($valueCond)) {
-                    $value = $valueCond[0];
-                    $condition = $this->searchDefaultCondition;
-
-                    if (!empty($valueCond[1])) {
-                        $condition = $valueCond[1];
-                    }
-
-                    $modelReturn = $modelReturn->where($column, $condition, $value);
-                }
+                $modelReturn = $this->_parseParam($searchExplodeItem, $modelReturn);
             }
         }
 
         return $modelReturn;
+    }
+    
+    // ----------------------------------------------------------------------
+
+    protected function _parseParam($searchExplodeItem, $modelReturn) {
+        preg_match_all('/([\w]+)\(([^\)]+)\)/', $searchExplodeItem, $allModifiersArr);
+        $column = $allModifiersArr[1][0];
+        $valueCond = explode($this->searchSecondDelimiter, $allModifiersArr[2][0], 3);
+        if (empty($valueCond)) {
+            throw new InvalidArgumentException;
+        }
+        $value = $valueCond[0];
+        $condition = $this->searchDefaultCondition;
+        $type = $this->whereTypeDefault;
+        if (!empty($valueCond[1])) {
+            $condition = $valueCond[1];
+        }
+        if (!empty($valueCond[2])) {
+            $type = strtoupper($valueCond[2]);
+        }
+        if ($type === "AND") {
+            return $modelReturn->where($column, $condition, $value);
+        }
+        return $modelReturn->orWhere($column, $condition, $value);
     }
 
 }
