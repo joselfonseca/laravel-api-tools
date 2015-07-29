6a5e807af6e927426e0e9bec169ae5de6aed8d86
jose fonseca <jose@ditecnologia.com>
2015-07-28 21:06:44 -0500

    initializing tests

--- tests/TestExceptions.php
+++ tests/TestExceptions.php
@@ -8,4 +8,20 @@ class TestExceptions extends Orchestra\Testbench\TestCase{
         return ['Joselfonseca\LaravelApiTools\LaravelApiToolsServiceProvider'];
     }
 
+    /**
+     * @expectedException     Joselfonseca\LaravelApiTools\Exceptions\AclException
+     */
+    public function testAclException()
+    {
+        throw new Joselfonseca\LaravelApiTools\Exceptions\AclException();
+    }
+
+    /**
+     * @expectedException     Joselfonseca\LaravelApiTools\Exceptions\ApiModelNotFoundException
+     */
+    public function testApiModelNotFoundException()
+    {
+        throw new Joselfonseca\LaravelApiTools\Exceptions\ApiModelNotFoundException();
+    }
+
 }
